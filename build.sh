a76f427346bdad4dbdc13056c9ede90efe02029c
Joel Bradshaw <cincodenada@gmail.com>
2017-04-14 16:27:49 -0700

    Make build script fancier

--- build.sh
+++ build.sh
@@ -1,1 +1,7 @@
-xelatex resume.tex && xdg-open resume.pdf
+#!/bin/bash
+SCPDEST="cincodenada.com:~/cincodenada/stuff/"
+xelatex resume.tex
+CURBRANCH=$(git rev-parse --abbrev-ref HEAD)
+cp resume.pdf resume-$CURBRANCH.pdf
+scp -C resume-$CURBRANCH.pdf $SCPDEST
+xdg-open resume.pdf &
