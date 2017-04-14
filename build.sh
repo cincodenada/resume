2b45e283a5144617438583d5a72f835dbcaca28d
Joel Bradshaw <cincodenada@gmail.com>
2017-04-14 16:34:17 -0700

    Split build/upload scripts

--- build.sh
+++ build.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-SCPDEST="cincodenada.com:~/cincodenada/stuff/"
 xelatex resume.tex
-CURBRANCH=$(git rev-parse --abbrev-ref HEAD)
-cp resume.pdf resume-$CURBRANCH.pdf
-scp -C resume-$CURBRANCH.pdf $SCPDEST
 xdg-open resume.pdf &
